08c9c3d8960bc288c940060150a7a914b522fcec
Asmita OJha <asmitaojha05@gmail.com>
2024-11-09 13:47:18 +0545

    Learning thread

--- src/Practice_Thread.java
+++ src/Practice_Thread.java
@@ -9,6 +9,7 @@ class B extends Thread {
     public void run(){
         for(int i=0;i<5; i++){
             System.out.println("Hello Baba");
+
         }
     }
 }
--- src/Runnable_Thread_Example.java
+++ src/Runnable_Thread_Example.java
@@ -9,6 +9,7 @@ class Second implements Runnable{
     public void run(){
         for(int i=0; i<50;i++){
             System.out.println("Hello Monkey");
+
         }
     }
 }
